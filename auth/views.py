99eab780d842f1797a4cd4ac2d624b51ba643b5f
Rohit Kumar <rkumar0206@users.noreply.github.com>
2021-05-06 18:48:04 +0000

    Added functionality by which user can change or update the password

--- auth/views.py
+++ auth/views.py
@@ -1,14 +1,21 @@
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from auth.serializers import MyTokenObtainPairSerializer
 from django.contrib.auth.models import User
-from auth.serializers import RegisterSerializer
+from auth.serializers import RegisterSerializer, ChangePasswordSerializer
 from rest_framework import generics
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
